1974720f74a51cd7a869f940d75a91de78125070
Karl Eklund <localpart@gmail.com>
2021-02-19 01:54:12 +0100

    Remove Erlang-latest because I did it wrong

--- default.nix
+++ default.nix
@@ -1,18 +1,14 @@
 let
-  # Import sources
   sources = import ./nix/sources.nix;
 
-  erlang_latest = import ./erlang-23.2.5;
   test_hello = import ./test-hello;
 
   pkgs = import sources.nixpkgs {
     overlays = [
       (self: super: {
-        inherit erlang_latest;
         inherit test_hello;
       })
     ];
   };
 
-# And return that specific nixpkgs
 in pkgs
